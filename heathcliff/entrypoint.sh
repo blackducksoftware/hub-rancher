c985c85ca10be1534fbe668b85bbb74148911dbc
Murat Kumykov <mkumykov@blackducksoftware.com>
2018-08-23 16:40:47 -0400

    4.8.1

--- heathcliff/entrypoint.sh
+++ heathcliff/entrypoint.sh
@@ -9,4 +9,16 @@ mkdir -p /tmp/www/cgi-bin &&\
     chmod +x /tmp/www/cgi-bin &&\
     /opt/crate/heathcliff/healthchecks/busybox httpd -h /tmp/www -p 8000
 
-sh $ENTRYPOINT
+# Postgres will insist on running as PID 1 so we let it
+#
+
+case "$ENTRYPOINT" in
+    *postgres*)
+    # Postgres should be started with EXEC to allow PID 1
+    exec $ENTRYPOINT
+    ;;
+    *)
+    # In all other cases ensure sh is PID 1
+    sh -c "$ENTRYPOINT"
+    ;;
+esac
