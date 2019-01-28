d0ccd29131ef08d95f94b128d93eb75d3205a107
Murat Kumykov <mkumykov@blackducksoftware.com>
2019-01-28 16:14:05 -0500

    2018.12.2 deployment

--- heathcliff/healthchecks/wrapper.sh
+++ heathcliff/healthchecks/wrapper.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+export NGINXPID=$(ps -ef | grep nginx | grep master | grep -v grep | awk '{print $1}')
+test -z $NGINXPID || kill -HUP $NGINXPID
+
 if $HEALTHCHECK ; then
     echo "healthy"
 else
